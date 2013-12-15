fb41976ce23e9fab229595f555f30c854cc6e137
kwakwak <ronihcohen@gmail.com>
2013-12-15 17:49:21 +0200

    reindent

--- index.php
+++ index.php
@@ -1,81 +1,82 @@
 <html>
-	<head>
-		<meta charset="windows-1255">
-		<script src="http://code.jquery.com/jquery-latest.min.js"
-		        type="text/javascript"></script>
-		<script type="text/javascript">
-		$( document ).ready(function() {
+<head>
+	<meta charset="windows-1255">
+	<script src="http://code.jquery.com/jquery-latest.min.js"
+	type="text/javascript"></script>
+	<script type="text/javascript">
+	$( document ).ready(function() {
 
-			$("p").filter(function() {
-			    return  $(this).attr("level") > 0;
-			}).hide();
+		$("p").filter(function() {
+			return  $(this).attr("child") > 1;
+		}).hide();
 
-			$( "p" ).click(function() {
-				var child= parseInt($(this).attr("child"));
-				$(this).find('img').toggle().findChild(child).fadeChild(child);
-			});
+		$( "p" ).click(function() {
+			var child= parseInt($(this).attr("child"));
+			$(this).find('img').toggle().findChild(child).fadeChild(child);
+		});
 
-			jQuery.fn.fadeChild = function(child) {
-   				if ($(this).is(":visible")) {
-
-   				childObj = $(this).findChild(child);
+		jQuery.fn.fadeChild = function(child) {
+			if ($(this).is(":visible")) {
+				childObj = $(this).findChild(child);
 
 				childObj.each(function() {
 					$(this).loopChild();
 				});
 
-   				} else {
-   					$(this).fadeIn();	
-   				}
-			};
+			} else {
+				$(this).fadeIn();	
+			}
+		};
 
-			jQuery.fn.changeToExpand = function() {
-				$(this).find("img.collapse").hide();
-				$(this).find("img.expand").show();
-			};
+		jQuery.fn.changeToExpand = function() {
+			$(this).find("img.collapse").hide();
+			$(this).find("img.expand").show();
+		};
 
-			jQuery.fn.findChild = function(child) {
-				return $("p").filter(function() {
-				     return  $(this).attr("parent") == child;
-				});
-			};
+		jQuery.fn.findChild = function(child) { // find direct children
+			return $("p").filter(function() {
+				return  $(this).attr("parent") == child;
+			});
+		};
 
-			jQuery.fn.loopChild = function(childObj) {
-			
-					childObj = $(this);
-	
-				if (childObj.is(":visible"))
-					{	
-					childObj.fadeOut().changeToExpand();
-					var child= parseInt(childObj.attr("child"));
-					childObj = $(this).findChild(child);
+		jQuery.fn.loopChild = function(childObj) { // finds all children
 
-					childObj.each(function() {
+			childObj = $(this);
+
+			if (childObj.is(":visible"))
+			{	
+				childObj.fadeOut().changeToExpand();
+				var child= parseInt(childObj.attr("child"));
+				childObj = jQuery.fn.findChild(child);
+
+				childObj.each(function() {
 					$(this).loopChild();
 				});
-					}
-			};
+			}
+		};
 
-			
-		});
-		</script>
 
-		<style>
-		body
-		{
-		direction:rtl;
-		}
-		</style>
-	</head>
-	<body>
+	});
+</script>
+
+<style>
+body{
+	direction:rtl;
+}
+p {
+	cursor:pointer;
+}
+</style>
+</head>
+<body>
 
 	<?php
-	
-		$child = null;
-		$level= 0;
 
-		if (($handle = fopen("ctc.csv", "r")) !== FALSE) {
-		    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	$child = null;
+	$level= 0;
+
+	if (($handle = fopen("ctc.csv", "r")) !== FALSE) {
+		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 
 			$parent = $data[1];
 
@@ -88,14 +89,16 @@
 			$child = $data[0];
 			$lvlArr[$child]=$level;
 
-			echo ("<p style='padding-right:".$level . "em' level='".$level ."'' child='". $child ."' parent='" .$parent ."'>");
+			echo ("<p style='padding-right:".$level . "em'  
+				child='". $child ."' 
+				parent='" .$parent ."'>");
 			echo (" <img src='expand.png' class='expand'> ");
 			echo (" <img src='collapse.png' class='collapse' style='display:none'> ");
-	    	echo ($data[2]."</p>");
-		     
-		    }
-		    fclose($handle);
+			echo ($data[2]."</p>");
+
 		}
+		fclose($handle);
+	}
 	?>
-	</body>
+</body>
 </html>
